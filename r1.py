0d77e7e4a9fa5a07858cbc39e8af4cec2328497e
lauw14 <lauw14@hotmail.com>
2023-01-05 19:18:17 +0800

    done with r1.py

---
+++ r1.py
@@ -0,0 +1,32 @@
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8-sig') as f:
+        lines = []
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+def convert(lines):
+    new = []
+    person = None #代表「無」來當預設值，其他語言寫成Null
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:  # 假如person有值，也就是if person != None:
+            new.append(person + ': ' + line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+main()
